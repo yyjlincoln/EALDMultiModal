f9ec68338feac60a663c65758fdec10a8da1dc84
Lincoln Yan <yyjlincoln@gmail.com>
2021-03-07 11:28:14 +1100

    Update: Archive before delete

---
+++ src/views/AppFrame.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <frame>
+      <router-view></router-view>
+    </frame>
+  </div>
+</template>
+
+<script>
+import frame from "../components/frame.vue";
+export default {
+  components: { frame },
+};
+</script>
+
+<style>
+</style>
