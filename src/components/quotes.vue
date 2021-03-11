4196be88614f3290bd6180b46972d43cefd5d628
yyjlincoln <yyjlincoln@gmail.com>
2021-03-11 11:00:50 +1100

    Update

---
+++ src/components/quotes.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mx-auto text-center italic text-gray-800 my-4 flex flex-col">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/explore.vue
+++ src/views/explore.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nam-page v-if="completed" :unbounded="false">
+    <nam-page v-if="completed" :unbounded="true" class="max-w-7xl">
       <paragraph-content>
         <div class="flex flex-col">
           <span class="text-5xl font-bold">
@@ -24,10 +24,25 @@
         </subtitle>
 
         <pcontent>
-          <p>""</p>
+          <quotes>
+            <span>"i possess two countries, </span>
+            <span>or else</span>
+            <span>i possess neither"</span>
+            <span>-- The Double Man</span>
+          </quotes>
+          <p>
+            Many people with dual identity has this feeling. Probably you do as
+            well. We always naively feel like we can handle both identities at
+            the same time, yet without knowing, our identity shifts as our
+            surronding environment changes,
+            <dynamic-highlight
+              >just like the beach at Bondi Junction -- no matter how robust you
+              structure is, it can never conquer the nature, and it is gradually
+              eroded over time.</dynamic-highlight
+            >
+          </p>
+          <img class="rounded-md my-5 object-fit" src="https://images.unsplash.com/photo-1563003150-c142eb86a66e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2251&q=80"/>
         </pcontent>
-
-
       </paragraph-content>
     </nam-page>
     <nam-page v-if="!completed" :global_centered="true">
@@ -58,6 +73,7 @@ import namPage from "../components/nam-page.vue";
 import namText from "../components/nam-text.vue";
 import ParagraphContent from "../components/paragraph-content.vue";
 import Subtitle from "../components/subtitle.vue";
+import Quotes from "../components/quotes.vue";
 // import ealdTag from "../components/eald-tag.vue";
 // import { gsap } from "gsap";
 
@@ -69,7 +85,8 @@ export default {
     DynamicHighlight,
     ParagraphContent,
     Subtitle,
-    pcontent
+    pcontent,
+    Quotes,
   },
   data: () => ({
     animatedText: "",
